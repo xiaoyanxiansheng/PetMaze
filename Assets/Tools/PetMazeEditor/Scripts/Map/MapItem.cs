eb989e010194454ce63be7f68272fbab4a2da44b
xiaoyanxiansheng <360507245@qq.com>
2019-04-25 02:00:29 +0800

    add csvTools

---
+++ Assets/Tools/PetMazeEditor/Scripts/Map/MapItem.cs
@@ -0,0 +1,40 @@
+﻿
+using UnityEngine;
+/// <summary>
+/// 地块数据
+/// </summary>
+namespace PetMaze
+{
+    public class MapItem : MonoBehaviour
+    {
+        [SerializeField] private int _pointX = 0;
+        [SerializeField] private int _pointY = 0;
+        [SerializeField] private EventWrap _mapEventWrap = new EventWrap();
+
+        public int PointX
+        {
+            get
+            {
+                return _pointX;
+            }
+
+            set
+            {
+                _pointX = value;
+            }
+        }
+
+        public int PointY
+        {
+            get
+            {
+                return _pointY;
+            }
+
+            set
+            {
+                _pointY = value;
+            }
+        }
+    }
+}
---
+++ Assets/Tools/PetMazeEditor/Scripts/Toos/CsvTools.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+/// <summary>
+/// 参考 https://blog.csdn.net/eazey_wj/article/details/78821193
+/// </summary>
+namespace PetMaze
+{
+    public class CsvData
+    {
+        private static CsvData _instance;
+
+        private List<string> _keyList = new List<string>();
+        private List<List<string>> _valueList = new List<List<string>>();
+
+        public static CsvData Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new CsvData();
+                return _instance;
+            }
+        }
+
+        public void Open(string path)
+        {
+
+        }
+
+        public void Create()
+        {
+
+        }
+
+        public void Save()
+        {
+
+        }
+
+        public void Modify(int x, int y,string value)
+        {
+            if (IsValid(x, y))
+                return;
+            _valueList[x][y] = value;
+        }
+
+        public bool IsValid(int x, int y)
+        {
+            bool isValid = false;
+
+            if (y >= 0 && y < _keyList.Count)
+                if (x >= 0 && x < _valueList.Count && y < _valueList[x].Count)
+                    isValid = true;
+
+            return isValid;
+        }
+    }
+}
